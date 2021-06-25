17589eed2b159f81a1943dd33129760501b0256f
Jakub Wysocki <jakubwysocki@protonmail.com>
2021-06-25 14:49:14 +0200

    forked mqtt server and client from another project

--- MQTTClientServer/MqttClient/Program.cs
+++ MQTTClientServer/MqttClient/Program.cs
@@ -1,18 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace server
+namespace MqttClient
 {
-    static class Program
+    using System;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The main program.
+    /// </summary>
+    internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        ///     The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
