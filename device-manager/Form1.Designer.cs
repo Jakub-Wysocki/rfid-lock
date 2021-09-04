1344d1d00467ea06d76a0967f9e9d5c3dd6b1187
Jakub Wysocki <jakubwysocki@protonmail.com>
2021-09-04 13:18:58 +0200

    Add Basic Control to application

---
+++ device-manager/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace device_manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MqttLog = new System.Windows.Forms.TextBox();
+            this.TokenListBox = new System.Windows.Forms.ListBox();
+            this.LogLabel = new System.Windows.Forms.Label();
+            this.CardNumberInputBox = new System.Windows.Forms.TextBox();
+            this.AcceptCardNumber = new System.Windows.Forms.Button();
+            this.RemoveTokenButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MqttLog
+            // 
+            this.MqttLog.Location = new System.Drawing.Point(12, 214);
+            this.MqttLog.Multiline = true;
+            this.MqttLog.Name = "MqttLog";
+            this.MqttLog.Size = new System.Drawing.Size(776, 224);
+            this.MqttLog.TabIndex = 0;
+            // 
+            // TokenListBox
+            // 
+            this.TokenListBox.FormattingEnabled = true;
+            this.TokenListBox.ItemHeight = 20;
+            this.TokenListBox.Location = new System.Drawing.Point(12, 12);
+            this.TokenListBox.Name = "TokenListBox";
+            this.TokenListBox.Size = new System.Drawing.Size(169, 164);
+            this.TokenListBox.TabIndex = 1;
+            // 
+            // LogLabel
+            // 
+            this.LogLabel.AutoSize = true;
+            this.LogLabel.Location = new System.Drawing.Point(12, 191);
+            this.LogLabel.Name = "LogLabel";
+            this.LogLabel.Size = new System.Drawing.Size(126, 20);
+            this.LogLabel.TabIndex = 2;
+            this.LogLabel.Text = "Log from devices:";
+            // 
+            // CardNumberInputBox
+            // 
+            this.CardNumberInputBox.Location = new System.Drawing.Point(187, 119);
+            this.CardNumberInputBox.Name = "CardNumberInputBox";
+            this.CardNumberInputBox.Size = new System.Drawing.Size(165, 27);
+            this.CardNumberInputBox.TabIndex = 3;
+            // 
+            // AcceptCardNumber
+            // 
+            this.AcceptCardNumber.Location = new System.Drawing.Point(187, 152);
+            this.AcceptCardNumber.Name = "AcceptCardNumber";
+            this.AcceptCardNumber.Size = new System.Drawing.Size(94, 29);
+            this.AcceptCardNumber.TabIndex = 4;
+            this.AcceptCardNumber.Text = "Add Token";
+            this.AcceptCardNumber.UseVisualStyleBackColor = true;
+            // 
+            // RemoveTokenButton
+            // 
+            this.RemoveTokenButton.Location = new System.Drawing.Point(187, 12);
+            this.RemoveTokenButton.Name = "RemoveTokenButton";
+            this.RemoveTokenButton.Size = new System.Drawing.Size(194, 29);
+            this.RemoveTokenButton.TabIndex = 5;
+            this.RemoveTokenButton.Text = "Remove Selected Token";
+            this.RemoveTokenButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RemoveTokenButton);
+            this.Controls.Add(this.AcceptCardNumber);
+            this.Controls.Add(this.CardNumberInputBox);
+            this.Controls.Add(this.LogLabel);
+            this.Controls.Add(this.TokenListBox);
+            this.Controls.Add(this.MqttLog);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MqttLog;
+        private System.Windows.Forms.ListBox TokenListBox;
+        private System.Windows.Forms.Label LogLabel;
+        private System.Windows.Forms.TextBox CardNumberInputBox;
+        private System.Windows.Forms.Button AcceptCardNumber;
+        private System.Windows.Forms.Button RemoveTokenButton;
+    }
+}
+
--- device-manager/Form1.cs
+++ device-manager/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace device_manager
@@ -16,6 +9,7 @@ namespace device_manager
         {
             InitializeComponent();
         }
+
 
     }
 }
