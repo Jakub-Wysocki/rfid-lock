e5557a556a69a3a77b33c34d32ee21ff304f3e6f
Jakub Wysocki <jakubwysocki@protonmail.com>
2021-09-04 12:06:29 +0200

    code lift

--- client/main/app_main.c
+++ client/main/app_main.c
@@ -27,28 +27,6 @@
 #include "driver/gpio.h"
 
 static pn532_t nfc;
-static EventGroupHandle_t s_wifi_event_group;
-
-static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
-{
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
-            esp_wifi_connect();
-            s_retry_num++;
-            ESP_LOGI(TAG_WIFI, "retry to connect to the AP");
-        } else {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-        }
-        ESP_LOGI(TAG_WIFI,"connect to the AP fail");
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG_WIFI, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-        s_retry_num = 0;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-    }
-}
 
 static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
 {
@@ -85,16 +63,16 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
             ESP_LOGI(MQTT_TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
             break;
         case MQTT_EVENT_DATA:
-            if(strcmp(event->topic, "/rfid/add")) {
+            if(!strcmp(event->topic, "/rfid/add")) {
                     //add
                     printf("DATA=%.*s\r\n", event->data_len, event->data);
                     ESP_LOGI(MQTT_TAG, "KEY %.*s ADDED!", event->data_len, event->data ); 
-            } else if(strcmp(event->topic, "/rfid/remove")) {
+            } else if(!strcmp(event->topic, "/rfid/remove")) {
                     
                     //remove
                     printf("DATA=%.*s\r\n", event->data_len, event->data);
                     ESP_LOGI(MQTT_TAG, "KEY %.*s REMOVED!", event->data_len, event->data); 
-            } else if(strcmp(event->topic, "/rfid/list"))  { 
+            } else if(!strcmp(event->topic, "/rfid/list"))  { 
                 
                     //publish list
                     printf("DATA=%.*s\r\n", event->data_len, event->data);
@@ -116,7 +94,6 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
     return ESP_OK;
 }
 
-
 //function handles access attempt. 
 void access_attempt()
 {
@@ -124,69 +101,6 @@ void access_attempt()
     //check wheather card has access
     //publish log with result
 
-}
-
-void wifi_init_sta(void)
-{
-    s_wifi_event_group = xEventGroupCreate();
-
-    ESP_ERROR_CHECK(esp_netif_init());
-
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
-
-
- 
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID,
-            .password = EXAMPLE_ESP_WIFI_PASS,
-            /* Setting a password implies station will connect to all security modes including WEP/WPA.
-             * However these modes are deprecated and not advisable to be used. Incase your Access point
-             * doesn't support WPA2, these mode can be enabled by commenting below line */
-	        //.threshold.authmode = WIFI_AUTH_WPA2_PSK,
-            .pmf_cfg = {
-                .capable = true,
-                .required = false
-            },
-        },
-    }; 
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_start() );
-    
-    ESP_LOGI(TAG_WIFI, "wifi_init_sta finished.");
-
-    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
-
-    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
-     * happened. */
-    if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(TAG_WIFI, "connected to ap SSID:%s password:%s",
-                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
-    } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(TAG_WIFI, "Failed to connect to SSID:%s, password:%s",
-                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
-    } else {
-        ESP_LOGE(TAG_WIFI, "UNEXPECTED EVENT");
-    }
-
-    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
-    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
-    vEventGroupDelete(s_wifi_event_group);
 }
 
 void nfc_task(void *pvParameter)
@@ -253,7 +167,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 static void mqtt_app_start(void)
 {
     esp_mqtt_client_config_t mqtt_cfg = {
-        .uri = "mqtt://192.168.1.100:1883",
+        .uri = CONFIG_BROKER_URL,
     };
 
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
@@ -263,14 +177,6 @@ static void mqtt_app_start(void)
 
 void app_main(void)
 {
-     //Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
-
     ESP_LOGI(MQTT_TAG, "[APP] Startup..");
     ESP_LOGI(MQTT_TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
     ESP_LOGI(MQTT_TAG, "[APP] IDF version: %s", esp_get_idf_version());
@@ -283,21 +189,13 @@ void app_main(void)
     esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
     esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
 
-    ESP_LOGI(TAG_WIFI, "ESP_WIFI_MODE_STA");
-
-    wifi_init_sta();
-
-    xTaskCreate(&nfc_task, "nfc_task", 4096, NULL, 4, NULL);
-
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
     ESP_ERROR_CHECK(example_connect());
 
-    mqtt_app_start();
+    mqtt_app_start(); //there could be an error with nfc task priority
+
+    //xTaskCreate(&nfc_task, "nfc_task", 4096, NULL, 4, NULL);
 }
--- client/main/app_main.h
+++ client/main/app_main.h
@@ -2,29 +2,13 @@
 #define APP_MAIN_H
 
 
-#define EXAMPLE_ESP_WIFI_SSID ""
-#define EXAMPLE_ESP_WIFI_PASS "" 
-#define EXAMPLE_ESP_MAXIMUM_RETRY 1
-
-#define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
-
 /* SPI CONFIG*/
 #define PN532_SCK 19
 #define PN532_MOSI 23
 #define PN532_SS 22
 #define PN532_MISO 25
 
-/* GPIO PINS*/
-#define GPIO_BUZZER 27
-#define GPIO_RED_LED 14
-
 static const char *MQTT_TAG = "MQTT";
-
-static const char *TAG_WIFI = "WiFi";
 static const char *TAG_RFID = "RFID";
-static int s_retry_num = 0;
-
-
 
 #endif //APP_MAIN_H
