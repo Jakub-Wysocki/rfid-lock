594d2e47c3823b82f4d6e06e39cf7330a4d43ab1
Jakub Wysocki <jakubwysocki@protonmail.com>
2021-09-07 22:37:41 +0200

    Graphical updates

--- device-manager/Form1.Designer.cs
+++ device-manager/Form1.Designer.cs
@@ -29,23 +29,15 @@ namespace device_manager
         /// </summary>
         private void InitializeComponent()
         {
-            this.MqttLog = new System.Windows.Forms.TextBox();
             this.TokenListBox = new System.Windows.Forms.ListBox();
             this.LogLabel = new System.Windows.Forms.Label();
             this.CardNumberInputBox = new System.Windows.Forms.TextBox();
             this.AcceptCardNumber = new System.Windows.Forms.Button();
             this.RemoveTokenButton = new System.Windows.Forms.Button();
             this.UpdateListButton = new System.Windows.Forms.Button();
-            this.SaveLog = new System.Windows.Forms.Button();
+            this.MqttLog = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // MqttLog
-            // 
-            this.MqttLog.Location = new System.Drawing.Point(12, 214);
-            this.MqttLog.Multiline = true;
-            this.MqttLog.Name = "MqttLog";
-            this.MqttLog.Size = new System.Drawing.Size(776, 224);
-            this.MqttLog.TabIndex = 0;
             // 
             // TokenListBox
             // 
@@ -53,13 +45,13 @@ namespace device_manager
             this.TokenListBox.ItemHeight = 20;
             this.TokenListBox.Location = new System.Drawing.Point(12, 12);
             this.TokenListBox.Name = "TokenListBox";
-            this.TokenListBox.Size = new System.Drawing.Size(169, 164);
+            this.TokenListBox.Size = new System.Drawing.Size(200, 164);
             this.TokenListBox.TabIndex = 1;
             // 
             // LogLabel
             // 
             this.LogLabel.AutoSize = true;
-            this.LogLabel.Location = new System.Drawing.Point(12, 191);
+            this.LogLabel.Location = new System.Drawing.Point(12, 179);
             this.LogLabel.Name = "LogLabel";
             this.LogLabel.Size = new System.Drawing.Size(126, 20);
             this.LogLabel.TabIndex = 2;
@@ -67,14 +59,14 @@ namespace device_manager
             // 
             // CardNumberInputBox
             // 
-            this.CardNumberInputBox.Location = new System.Drawing.Point(187, 119);
+            this.CardNumberInputBox.Location = new System.Drawing.Point(218, 114);
             this.CardNumberInputBox.Name = "CardNumberInputBox";
-            this.CardNumberInputBox.Size = new System.Drawing.Size(165, 27);
+            this.CardNumberInputBox.Size = new System.Drawing.Size(235, 27);
             this.CardNumberInputBox.TabIndex = 3;
             // 
             // AcceptCardNumber
             // 
-            this.AcceptCardNumber.Location = new System.Drawing.Point(187, 152);
+            this.AcceptCardNumber.Location = new System.Drawing.Point(218, 147);
             this.AcceptCardNumber.Name = "AcceptCardNumber";
             this.AcceptCardNumber.Size = new System.Drawing.Size(94, 29);
             this.AcceptCardNumber.TabIndex = 4;
@@ -84,9 +76,9 @@ namespace device_manager
             // 
             // RemoveTokenButton
             // 
-            this.RemoveTokenButton.Location = new System.Drawing.Point(187, 12);
+            this.RemoveTokenButton.Location = new System.Drawing.Point(218, 12);
             this.RemoveTokenButton.Name = "RemoveTokenButton";
-            this.RemoveTokenButton.Size = new System.Drawing.Size(194, 29);
+            this.RemoveTokenButton.Size = new System.Drawing.Size(235, 29);
             this.RemoveTokenButton.TabIndex = 5;
             this.RemoveTokenButton.Text = "Remove Selected Token";
             this.RemoveTokenButton.UseVisualStyleBackColor = true;
@@ -94,36 +86,49 @@ namespace device_manager
             // 
             // UpdateListButton
             // 
-            this.UpdateListButton.Location = new System.Drawing.Point(187, 67);
+            this.UpdateListButton.Location = new System.Drawing.Point(218, 47);
             this.UpdateListButton.Name = "UpdateListButton";
-            this.UpdateListButton.Size = new System.Drawing.Size(194, 29);
+            this.UpdateListButton.Size = new System.Drawing.Size(235, 29);
             this.UpdateListButton.TabIndex = 6;
             this.UpdateListButton.Text = "Update List";
             this.UpdateListButton.UseVisualStyleBackColor = true;
             this.UpdateListButton.Click += new System.EventHandler(this.UpdateListButton_Click);
             // 
-            // SaveLog
+            // MqttLog
             // 
-            this.SaveLog.Location = new System.Drawing.Point(694, 179);
-            this.SaveLog.Name = "SaveLog";
-            this.SaveLog.Size = new System.Drawing.Size(94, 29);
-            this.SaveLog.TabIndex = 7;
-            this.SaveLog.Text = "button1";
-            this.SaveLog.UseVisualStyleBackColor = true;
+            this.MqttLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MqttLog.FormattingEnabled = true;
+            this.MqttLog.ItemHeight = 20;
+            this.MqttLog.Location = new System.Drawing.Point(12, 202);
+            this.MqttLog.Name = "MqttLog";
+            this.MqttLog.Size = new System.Drawing.Size(455, 184);
+            this.MqttLog.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(318, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 29);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.SaveLog);
+            this.ClientSize = new System.Drawing.Size(482, 403);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MqttLog);
             this.Controls.Add(this.UpdateListButton);
             this.Controls.Add(this.RemoveTokenButton);
             this.Controls.Add(this.AcceptCardNumber);
             this.Controls.Add(this.CardNumberInputBox);
             this.Controls.Add(this.LogLabel);
             this.Controls.Add(this.TokenListBox);
-            this.Controls.Add(this.MqttLog);
+            this.MinimumSize = new System.Drawing.Size(500, 450);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -132,15 +137,14 @@ namespace device_manager
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox MqttLog;
         private System.Windows.Forms.ListBox TokenListBox;
         private System.Windows.Forms.Label LogLabel;
         private System.Windows.Forms.TextBox CardNumberInputBox;
         private System.Windows.Forms.Button AcceptCardNumber;
         private System.Windows.Forms.Button RemoveTokenButton;
         private System.Windows.Forms.Button UpdateListButton;
-        private System.Windows.Forms.Button SaveLog;
+        private System.Windows.Forms.ListBox MqttLog;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- device-manager/Form1.cs
+++ device-manager/Form1.cs
@@ -2,41 +2,53 @@
 using System.Collections.Generic;
 using System.Windows.Forms;
 using MQTTnet;
-using Serilog;
-using Serilog.Sinks.WinForms;
+using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet.Server;
+using System.Text;
+using System.Linq;
 
 namespace device_manager
 {
     public partial class Form1 : Form
     {
-        private readonly MqttBroker broker;
-
+        private IMqttServer mqttServer;
+    
         public Form1()
         {
             InitializeComponent();
-            broker = new();
+            BrokerInit();
         }
-
-   /*     private void HandleReceivedApplicationMessage(MqttApplicationMessageReceivedEventArgs x)
-        {
-          
-                MessageBox.Show(x.ApplicationMessage.Topic.ToString());
-            
-        }*/
 
 
         private void AcceptCardNumber_Click(object sender, EventArgs e)
         {
-            TokenListBox.Items.Add(CardNumberInputBox.Text.ToString());
+            string cardNumber = CardNumberInputBox.Text.ToString();
+
+            
+
+
+            if (!(cardNumber.All(char.IsDigit) &&  !String.IsNullOrEmpty(cardNumber)))
+            {
+                MessageBox.Show("Only numbers are allowed");
+                return;
+            }
+            else if(TokenListBox.Items.Contains(cardNumber))
+            {
+                MessageBox.Show("Number already on the list");
+                return;
+            }
+
+            TokenListBox.Items.Add(cardNumber);
 
             var message = new MqttApplicationMessageBuilder()
                 .WithTopic("/rfid/add")
-                .WithPayload(CardNumberInputBox.Text.ToString())
+                .WithPayload(cardNumber)
                 .WithExactlyOnceQoS()
                 .WithRetainFlag()
                 .Build();
 
-            broker.PublishMessege(message);
+            PublishMessege(message);
          
         }
 
@@ -51,7 +63,7 @@ namespace device_manager
                 .WithRetainFlag()
                 .Build();
 
-                broker.PublishMessege(message);
+                PublishMessege(message);
 
                 TokenListBox.Items.Remove(TokenListBox.SelectedItem);
 
@@ -68,7 +80,138 @@ namespace device_manager
                 .WithRetainFlag()
                 .Build();
 
-            broker.PublishMessege(message);
+            PublishMessege(message);
         }
+
+        void AppendLog(string messege)
+        {
+            var time = DateTime.Now.ToString("T");
+
+            if (MqttLog.InvokeRequired)
+            {
+                MqttLog.Invoke(new Action<string>(AppendLog), time + " " + messege);
+            }
+                
+            else
+            {
+               MqttLog.Items.Add(time + " " + messege);
+            }
+                
+        }
+
+        void AppendTokenBox(string messege)
+        {
+
+            if (TokenListBox.InvokeRequired)
+                TokenListBox.Invoke(new Action<string>(AppendTokenBox), messege);
+            else
+                TokenListBox.Items.Add(messege);
+
+        }
+
+        void ClearTokenBox()
+        {
+
+            if (TokenListBox.InvokeRequired)
+                TokenListBox.Invoke(new Action(ClearTokenBox));
+            else
+                TokenListBox.Items.Clear();
+
+        }
+
+        private async void BrokerInit()
+        {
+            // Configure MQTT server.
+            var optionsBuilder = new MqttServerOptionsBuilder()
+                .WithConnectionBacklog(100)
+                .WithDefaultEndpointPort(1883)
+                .WithConnectionValidator(OnNewConnection)
+                .WithApplicationMessageInterceptor(OnNewMessage);
+
+            mqttServer = new MqttFactory().CreateMqttServer();
+            await mqttServer.StartAsync(optionsBuilder.Build());
+
+            //mqttServer.UseApplicationMessageReceivedHandler(this.HandleReceivedApplicationMessage);
+        }
+
+        private void OnNewConnection(MqttConnectionValidatorContext context)
+        {
+            AppendLog($"Client connected: {context.ClientId} ");
+
+            //AppendText($"Timestamp: {DateTime.Now:O} | Topic: {x.ApplicationMessage.Topic} | Payload: {x.ApplicationMessage.ConvertPayloadToString()} | QoS: {x.ApplicationMessage.QualityOfServiceLevel}");
+
+            //AppendText( );
+
+            
+        }
+        private void OnNewMessage(MqttApplicationMessageInterceptorContext context)
+        {
+            string message = context.ApplicationMessage.ConvertPayloadToString();
+            string topic = context.ApplicationMessage.Topic;            
+            string log = "Topic:  " + topic + "Message: " + message;
+
+           switch (topic)
+            {
+                case "/topic/log":
+                    AppendLog(log);
+                    break;
+
+                case "/topic/remove":
+
+                    if (message == "REMOVED")
+                    {
+                        AppendLog(log);
+                    }
+                    else if (message == "NO DEVICES")
+                    {
+                        AppendLog(log);
+                        ClearTokenBox();
+                    }
+
+                   break;
+                case "/topic/add":
+
+                    if (message.StartsWith("ADDED DEVICE"))
+                    {
+                        AppendLog(log);
+                    }
+                    else
+                        AppendLog(log);
+
+                    break;
+                case "/topic/list":
+
+                    if (message == "NO DEVICES")
+                    {
+                        AppendLog(log);
+                        ClearTokenBox();
+                    }
+                    else
+                    {
+                        string parsedMessage = System.Text.RegularExpressions.Regex.Replace(message, "[^0-9]", "").Trim();
+
+                        if(!TokenListBox.Items.Contains(parsedMessage))
+                            AppendTokenBox(parsedMessage);
+
+                        AppendLog("Updated from device" + parsedMessage);
+
+                    }
+                    break;
+
+            }
+
+            //var payload = context.ApplicationMessage?.Payload == null ? null : Encoding.UTF8.GetString(context.ApplicationMessage?.Payload);
+
+
+        }
+
+        private async void PublishMessege(MqttApplicationMessage message)
+        {
+
+            await mqttServer.PublishAsync(message, CancellationToken.None);
+
+        }
+
+
     }
 }
